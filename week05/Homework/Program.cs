75acdd3d58e80ccec1cbcfe53fff769c797acaae
Julio Cesar Celon Altamirano <grolain@hotmail.com>
2025-03-31 07:28:55 -0600

    W05 Learning Activity: Inheritance

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        Assignment assignment1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        Console.WriteLine();
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
