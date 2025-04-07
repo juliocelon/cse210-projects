0783ac30892179ac1cd628dca830652f119038d8
Julio Cesar Celon Altamirano <grolain@hotmail.com>
2025-04-07 08:02:40 -0600

    W06 Learning Activity: Polymorphism

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,5 +5,22 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        Square square = new Square("blue", 2);
+        Rectangle rectangle = new Rectangle("red", 6, 1.5);
+        Circle circle = new Circle("green", 3);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach(Shape shape in shapes)
+        {
+            Console.WriteLine($"Color : {shape.GetColor()}");
+            Console.WriteLine($"Area : {shape.GetArea()}");
+        }
+
+
     }
 }
